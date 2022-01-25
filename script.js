26bc4b0e0cc870f86c8c808ac972148b383a864b
leonidbachkutinskiy <leo4135@yandex.ru>
2022-01-25 19:58:52 +0300

    new

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".third").click(function () {
+        $(".third").animate({
+            top: "15px",
+            width: "200px",
+            marginLeft: "-100px"
+        }, 800);
+    })
+})
